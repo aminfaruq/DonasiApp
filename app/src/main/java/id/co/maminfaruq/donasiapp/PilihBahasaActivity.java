22bc87ca489f5710cb32562f7a4709f748ef9a01
Ziad Syahrul <ziadsm24@gmail.com>
2019-05-07 11:38:42 +0700

    layouting activity pilih bahasa

---
+++ app/src/main/java/id/co/maminfaruq/donasiapp/PilihBahasaActivity.java
@@ -0,0 +1,13 @@
+package id.co.maminfaruq.donasiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PilihBahasaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_bahasa);
+    }
+}
